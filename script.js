13785a9d792f7db935af869db4c545262591a261
giorgi <giorgikochuashvili2002@gmail.com>
2022-12-06 04:52:56 +0400

    added 2 pages

---
+++ script.js
@@ -0,0 +1,11 @@
+// const toggleContainer = document.querySelector(".toggle-container");
+// const toggleCircle = document.querySelector(".toggle-circle");
+// let toggled = false;
+// toggleContainer.addEventListener("click", function () {
+// 	if (!toggled) {
+// 		toggleCircle.classList.add("move-circle");
+// 	} else {
+// 		toggleCircle.classList.remove("move-circle");
+// 	}
+// 	toggled = !toggled;
+// });

96e42a80779b6f9599974e3302cd2741ad0242c6
giorgi <giorgikochuashvili2002@gmail.com>
2022-12-07 04:11:18 +0400

    fixes

--- script.js
+++ script.js
@@ -9,3 +9,25 @@
 // 	}
 // 	toggled = !toggled;
 // });
+
+// this changes numbers on buttons
+let pageNum = 1;
+const backBtn = document.querySelector(".back-btn");
+const nextBtn = document.querySelector(".next-btn");
+const pages = [...document.querySelectorAll(".btn-round")];
+console.log(pages);
+
+nextBtn.addEventListener("click", () => {
+	if (pageNum < 4 && pageNum >= 0) {
+		let currentPage = pages[pageNum];
+		pages.forEach((page) => {
+			page.classList.remove("active-btn");
+		});
+		pageNum++;
+		currentPage.classList.add("active-btn");
+		console.log(currentPage);
+	} else {
+		console.log(`${pageNum}`);
+	}
+});
+// end

d300a982a79da8d094e2e5598e5627cb48f99ff6
giorgi <giorgikochuashvili2002@gmail.com>
2022-12-18 23:45:21 +0400

    removed logs

--- add-on.js
+++ add-on.js
@@ -1,38 +1,33 @@
-import { monthly } from "./year-monthToggle.js";
+import { monthly } from './year-monthToggle.js';
 export let addOnsMonth = {};
 export let addOnsYear = {};
 const check = function (e) {
-	let t = e.target;
-	let add = t.closest(".add-on-btn");
-	let checkB = add.querySelector(".online-service");
-	console.log(t);
-	console.log(add);
-	console.log(checkB);
-	if (monthly) {
-		if (checkB.checked === false) {
-			console.log("a");
-			checkB.checked = true;
-			let dataName = checkB.dataset.box;
-			let dataPrice = checkB.dataset.addmonthprice;
-			addOnsMonth[dataName] = dataPrice;
-		} else {
-			checkB.checked = false;
-			let dataName = checkB.dataset.box;
-			delete addOnsMonth[dataName];
-		}
-	}
-	if (!monthly) {
-		if (checkB.checked === false) {
-			console.log("a");
-			checkB.checked = true;
-			let dataName = checkB.dataset.box;
-			let dataPrice = checkB.dataset.addyearprice;
-			addOnsYear[dataName] = dataPrice;
-		} else {
-			checkB.checked = false;
-			let dataName = checkB.dataset.box;
-			delete addOnsYear[dataName];
-		}
-	}
+  let t = e.target;
+  let add = t.closest('.add-on-btn');
+  let checkB = add.querySelector('.online-service');
+  if (monthly) {
+    if (checkB.checked === false) {
+      checkB.checked = true;
+      let dataName = checkB.dataset.box;
+      let dataPrice = checkB.dataset.addmonthprice;
+      addOnsMonth[dataName] = dataPrice;
+    } else {
+      checkB.checked = false;
+      let dataName = checkB.dataset.box;
+      delete addOnsMonth[dataName];
+    }
+  }
+  if (!monthly) {
+    if (checkB.checked === false) {
+      checkB.checked = true;
+      let dataName = checkB.dataset.box;
+      let dataPrice = checkB.dataset.addyearprice;
+      addOnsYear[dataName] = dataPrice;
+    } else {
+      checkB.checked = false;
+      let dataName = checkB.dataset.box;
+      delete addOnsYear[dataName];
+    }
+  }
 };
 export default check;
